ecf8b5bdd6d4878ecbbed00ff69d00fd270f07f3
Kees Briggs <kees.briggs@gmail.com>
2020-04-20 13:47:49 -0700

    add sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/login'
+  get 'sessions/welcome'
+  get 'authorized', to: 'sessions#page_requires_login'
+  resources :enrollments
+  resources :courses
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
